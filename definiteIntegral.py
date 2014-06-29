a035fcfa866f65777a8ee51489f28422e19aaac7
Rafael Pierri <rafael.pierri@metasix.com.br>
2014-06-29 00:10:59 -0300

    Now working with multiple processes.

--- definiteIntegral.py
+++ definiteIntegral.py
@@ -1,3 +1,5 @@
+from multiprocessing import Process, Pipe
+
 def power(expoent, base):
     i = 0
     result = 0
@@ -10,29 +12,47 @@ def power(expoent, base):
     return result
 
 def sum(array):
-    i = 1
+    i = 0
     result = 0
     while(i < array.__len__()):
         result = result + array[i]
         i = i + 1
     return result
 
-def definedIntegral(infLimit, supLimit, precision, expoent):
+def definedIntegral(infLimit, supLimit, precision, expoent, connection):
     i = 0
     array = []
     tmp = 0
     while(infLimit < supLimit):
         tmp = ((power(expoent, infLimit) + power(expoent, infLimit+precision))/2)*precision
         array.append(tmp)
-        #array.insert(i, tmp) 
         infLimit = infLimit + precision
         i = i + 1
-    return sum(array)
+    connection.send(sum(array))
+    connection.close()
     
 print("This software will calculate the Integral of a power function.")
 infLimit = float(raw_input("Write the lower limit:"))
 supLimit = float(raw_input("Write the upper limit:"))
 precision= float(raw_input("Write precision:"))
 expoent = int(raw_input("Write a power:"))
+workersNumber = int(raw_input("Write quantity of worker processes:"))
 
-print(definedIntegral(infLimit, supLimit, precision, expoent))
+workers = []
+
+i = 0
+while(i < workersNumber):
+    parent_conn, child_conn = Pipe()	
+    p = Process(target=definedIntegral, args=(infLimit+((supLimit-infLimit)/workersNumber)*i, infLimit+((supLimit-infLimit)/workersNumber)*(i+1), precision, expoent, child_conn,))
+    p.start()   
+    workers.append({'parent': parent_conn, 'child': child_conn, 'process': p})
+    i = i + 1	
+
+i = 0
+array = []
+while(i < workersNumber):
+    array.append(workers[i]['parent'].recv())
+    workers[i]['process'].join()
+    i = i + 1
+
+print(sum(array))
