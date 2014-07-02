d3eaaeced053b9b0ebfa63063535a377cbfd41ce
Ricardo Kagawa <ricardo.kagawa@metasix.com.br>
2014-07-02 11:49:41 -0300

    Improved defined_integral implementation and added unit tests.

--- definiteIntegral.py
+++ definiteIntegral.py
@@ -1,17 +1,19 @@
+from math import floor
 from multiprocessing import Pool
 
-def defined_integral(exponent, lower=0.0, upper=1.0, precision=0.001):
-    area = 0
-    while(lower < upper):
-        area += ((lower ** exponent + (lower + precision) ** exponent)) * precision / 2
-        lower = lower + precision
-    return area
+def defined_integral(exponent, lower=0.0, upper=1.0, precision=0.0001):
+    n = int(floor(float(upper - lower) / precision))
+    x = tuple(lower + i * precision for i in range(n))
+    y = map(lambda x: x ** exponent, x)
+    a = map(lambda y0, y1: (y0 + y1) * precision / 2.0, y[:-1], y[1:])
+    last = (y[-1] + upper ** exponent) * (upper - x[-1]) / 2.0
+    return sum(a) + last 
 
 def _defined_integral_task(args):
     'Expands arguments received from Pool to our API.'
     return defined_integral(*args)
 
-def mp_defined_integral(exponent, lower=0.0, upper=1.0, precision=0.001, workers=1):
+def mp_defined_integral(exponent, lower=0.0, upper=1.0, precision=0.0001, workers=4):
     'Numerically calculates a defined integral using several processes.'
 
     if workers == 1:
---
+++ test.py
@@ -0,0 +1,41 @@
+import unittest
+
+from definiteIntegral import defined_integral, mp_defined_integral
+
+class TestIntegral(unittest.TestCase):
+
+    def test_precision(self):
+        self.assertAlmostEqual(0.5, defined_integral(1, 0, 1, 1), delta=1)
+        self.assertAlmostEqual(0.5, defined_integral(1, 0, 1, 0.01), delta=0.01)
+        self.assertAlmostEqual(0.5, defined_integral(1, 0, 1, 0.0001), delta=0.0001)
+        self.assertAlmostEqual(0.5, defined_integral(1, 0, 1, 0.000001), delta=0.000001)
+
+    def test_lower(self):
+        self.assertAlmostEqual(-1.5, defined_integral(1, -2, 1), delta=0.0001)
+        self.assertAlmostEqual(0.0, defined_integral(1, -1, 1), delta=0.0001)
+        self.assertAlmostEqual(0.5, defined_integral(1, 0, 1), delta=0.0001)
+        self.assertAlmostEqual(0.095, defined_integral(1, 0.9, 1), delta=0.0001)
+
+    def test_upper(self):
+        self.assertAlmostEqual(0.005, defined_integral(1, 0, 0.1), delta=0.0001)
+        self.assertAlmostEqual(0.5, defined_integral(1, 0, 1), delta=0.0001)
+        self.assertAlmostEqual(2.0, defined_integral(1, 0, 2), delta=0.0001)
+        self.assertAlmostEqual(50.0, defined_integral(1, 0, 10), delta=0.0001)
+
+    def test_exponent(self):
+        self.assertAlmostEqual(0.5000, defined_integral(-2, 1, 2), delta=0.0001)
+        self.assertAlmostEqual(0.6931, defined_integral(-1, 1, 2), delta=0.0001)
+        self.assertAlmostEqual(0.8284, defined_integral(-0.5, 1, 2), delta=0.0001)
+        self.assertAlmostEqual(1.0000, defined_integral(0, 1, 2), delta=0.0001)
+        self.assertAlmostEqual(1.2190, defined_integral(0.5, 1, 2), delta=0.0001)
+        self.assertAlmostEqual(1.5000, defined_integral(1, 1, 2), delta=0.0001)
+        self.assertAlmostEqual(2.3333, defined_integral(2, 1, 2), delta=0.0001)
+
+    def test_mp(self):
+        self.assertAlmostEqual(0.5, mp_defined_integral(1, 0, 1, 0.001, 1), delta=0.001)
+        self.assertAlmostEqual(0.5, mp_defined_integral(1, 0, 1, 0.000001, 4), delta=0.000001)
+        self.assertAlmostEqual(0.5, mp_defined_integral(1, 0, 1, 0.000001, 16), delta=0.000001)
+
+if __name__ == '__main__':
+    unittest.main()
+
